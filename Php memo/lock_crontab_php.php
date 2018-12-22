767ba0d19c9a7fae24fb20e0a3372744aec252de
Oliver Guo <oliguo@users.noreply.github.com>
2018-12-22 10:55:06 +0800

    Update lock_crontab_php.php

--- Php memo/lock_crontab_php.php
+++ Php memo/lock_crontab_php.php
@@ -1,11 +1,11 @@
 <?php
-$log = dirname(__FILE__) . '/log_omb_omp_assign.txt';
-$lock = dirname(__FILE__) . '/lock_omb_omp_assign';
+$log = dirname(__FILE__) . '/log_.txt';
+$lock = dirname(__FILE__) . '/lock_';
 $log_file = fopen($log, 'a') or die('Cannot create log file');
 $lock_file = fopen($lock, 'w') or die('Cannot create lock file');
 if (flock($lock_file, LOCK_EX | LOCK_NB)) {
     //echo "is not locked,go to run";
-    fwrite($log_file, date('Y-m-d H:i:s').":checking"."\n");
+    fwrite($log_file, date('Y-m-d H:i:s').":exec now"."\n");
     
 } else {
     //echo "is locked,skip";
